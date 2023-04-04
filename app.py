7f0e03d2522a23553cc76b5d870b12af2104bf37
AArana670 <76699832+AArana670@users.noreply.github.com>
2023-04-04 12:54:13 +0200

    prueba de gestión de POST

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
+#index webpage
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+#receiving data
+@app.route('/signal', methods=['POST'])
+def addSignals():
+    data = print(request.form)
+
 
 if __name__ == '__main__':
    app.run()
