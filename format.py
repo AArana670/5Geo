1c67e77e38b774327f2f5dd042f56ae3467ee9cc
AArana670 <76699832+AArana670@users.noreply.github.com>
2023-04-20 10:21:58 +0200

    verificación básica de formato

--- format.py
+++ format.py
@@ -1,6 +1,24 @@
-def verify():
+def verify(json):
+    if "token" not in json:
+        return False
+    
+    if "signals" not in json:
+        return False
+    
+    for signal in json["signals"]:
+        if "dBm" not in signal:
+            return False
+        
+        if "ubiLat" not in signal:
+            return False
+        
+        if "ubiLong" not in signal:
+            return False
+        
+        if "moment" not in signal:
+            return False
 
-    return False
+    return True
 
 
 def build():
