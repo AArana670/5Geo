9cba4e52170fd87c9f79a12d34f87df7cc757db2
AArana670 <76699832+AArana670@users.noreply.github.com>
2023-04-22 02:49:57 +0200

    Indexación de datos del mapa recorregida

--- static/mapManager.js
+++ static/mapManager.js
@@ -6,7 +6,7 @@ var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 osm.addTo(map);
 
 fetch("http://157.245.35.106/signal")
-    .then((response) => response.json()).then(data => displayMap(JSON.parse(data)["signals"]));
+    .then((response) => response.json()).then(data => displayMap(JSON.parse(data["signals"])));
 
 /*displayMap([
     {ubiLat: 43.263, ubiLong: -2.955, dBm: 0.5, },
