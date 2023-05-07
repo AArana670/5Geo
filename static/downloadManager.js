d2dc52352b81ba3935a5a03c3b5107209f727331
AArana670 <76699832+AArana670@users.noreply.github.com>
2023-05-07 11:49:29 +0200

    Conversión a CSV

--- static/downloadManager.js
+++ static/downloadManager.js
@@ -1,19 +1,33 @@
+function ConvertToCSV (data) {
+    if (!data || data.length === 0) {
+      console.log("data was empty");
+      return;
+    }
+    let csv = [Object.keys(data[0]).slice(0).join(",")];
+    data.forEach(
+      item => csv.push(
+        Object.values(item).map(val => isNaN(val) ? '"'+val+'"':val).join(",")
+      )
+    )
+    csv=csv.join("\n");
+}
+
 function setMapData(data){
-    console.warn("Incomplete method:", "The method 'setMapData' has not been implemented, please fill the function before removing this warning");
-    const mapData = data;
+    mapData = data;
 }
 
 function setGraphData(data){
-    console.warn("Incomplete method:", "The method 'setGraphData' has not been implemented, please fill the function before removing this warning");
-    const graphData = data;
+    graphData = data;
 }
 
 function downloadMap(){
     console.warn("Incomplete method:", "The method 'downloadMap' has not been implemented, please fill the function before removing this warning");
-
+    downloadable = ConvertToCSV(mapData);
+    console.log(downloadable);
 }
 
 function downloadGraph(){
     console.warn("Incomplete method:", "The method 'downloadGraph' has not been implemented, please fill the function before removing this warning");
-
+    downloadable = ConvertToCSV(graphData);
+    console.log(downloadable);
 }
