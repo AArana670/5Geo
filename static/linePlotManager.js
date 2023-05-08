8ffa490b587765a0c0f52b8742b2baf1e1cb841e
AArana670 <76699832+AArana670@users.noreply.github.com>
2023-05-09 01:42:07 +0200

    Texto adicional en los puntos del gráfico

--- static/linePlotManager.js
+++ static/linePlotManager.js
@@ -6,7 +6,7 @@ function groupBy(arr, property) {  //https://stackoverflow.com/a/14696535
     }, {});
   }
 
-function displayGraph(graphData, title){
+function displayGraph(graphData){
 
     //group the signals by frequency
     var freqDivision = groupBy(graphData, "freq");
@@ -15,6 +15,7 @@ function displayGraph(graphData, title){
     const traceList = Object.entries(freqDivision).map(([freq, signals]) => {
         const timeline = new Set();
         const meanDBm = new Map();
+        const signalInfo = new Map();
     
         signals.forEach(signal => {
             timeline.add(signal.moment);
@@ -24,21 +25,27 @@ function displayGraph(graphData, title){
             const entry = meanDBm.get(signal.moment);
             entry.sum += signal.dBm;
             entry.count++;
+
+            signalInfo = "cId: " + signal.cId
+            if (signal.operator)
+                signalInfo += "\n operadora: " + signal.operator;
         });
     
         const x = Array.from(timeline);
         const y = x.map(time => meanDBm.get(time).sum / meanDBm.get(time).count);
+        const info = x.map(time => info.get(time));
     
         return {
             x,
             y,
+            text: info,
             type: 'scatter',
             name: freq + 'MHz',
         };
     });
 
     var layout = {
-        title,
+        //title: "Emisión EM en "+lat+", "+lng,
         xaxis:{  //https://plotly.com/python/range-slider/
             rangeselector:{
                 buttons:[
@@ -62,6 +69,8 @@ function displayGraph(graphData, title){
             },
             type:"date"
         }
+        /*plot_bgcolor:"#303030",
+        paper_bgcolor:"#FFF3"*/
     };
       
   
