b0a7d1c3b3c38aed66e2ebc2bd6f13bfcf720c5b
AArana670 <76699832+AArana670@users.noreply.github.com>
2023-05-08 19:31:30 +0200

    Escalado cuártico

--- static/dataFilter.js
+++ static/dataFilter.js
@@ -51,10 +51,10 @@ function withinRange(signal, objLat, objLong, zoom){
     if (round < 0)
         round = 0;
 
-    if (Math.abs(signal["ubiLat"] - objLat) > 0.01/(zoom^3))
+    if (Math.abs(signal["ubiLat"] - objLat) > 0.1/(zoom^4))
         return false;
 
-    if (Math.abs(signal["ubiLong"] - objLong) > 0.01/(zoom^3))
+    if (Math.abs(signal["ubiLong"] - objLong) > 0.1/(zoom^4))
         return false;
     
     return true;
